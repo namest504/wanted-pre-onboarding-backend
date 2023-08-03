35728c622d5a36e74c049e186fbec2a63cbc1065
List <61047602+namest504@users.noreply.github.com>
2023-08-03 21:17:58 +0900

    test: 테스트 코드 작성
    회원가입 Controller 테스트

--- src/test/java/com/lim1t/board/BoardApplicationTests.java
+++ src/test/java/com/lim1t/board/BoardApplicationTests.java
@@ -1,13 +1,8 @@
 package com.lim1t.board;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BoardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
